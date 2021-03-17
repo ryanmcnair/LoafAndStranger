cf46ae6e61934fcbd269542f94ff7ed038a59a1c
Ryan McNair <ryan.mcnair@gmail.com>
2021-03-16 21:01:47 -0500

    added dapper coding

---
+++ DataAccess/TopsRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoafAndStranger.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace LoafAndStranger.DataAccess
+{
+    public class TopsRepository
+    {
+
+        const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True";
+
+        public IEnumerable<Top> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From Tops";
+
+            var tops = db.Query<Top>(sql);
+
+            return tops;
+        }
+    }
+}
---
+++ Models/Top.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace LoafAndStranger.Models
+{
+    public class Top
+    {
+        public int id { get; set; }
+        public int NumberOfSeats { get; set; }
+        public bool Occupied { get; set; }
+    }
+}
