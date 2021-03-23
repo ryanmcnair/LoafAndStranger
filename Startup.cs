ea7183a23167fcf6651e12200c5814bb831ed196
Ryan McNair <ryan.mcnair@gmail.com>
2021-03-22 21:11:03 -0500

    had to fix the dumbest typo EVER

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace LoafAndStranger
             //services.AddSingleton<IConfiguration>();
 
             services.AddSingleton(Configuration);
-            services.AddTransient<StrangersRepository>()
+            services.AddTransient<StrangersRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
